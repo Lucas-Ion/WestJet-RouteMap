dc43368c68e2c011df96b31ef61e931dd63bbd02
Noah Bradley <nbradleyjoe19@gmail.com>
2023-01-23 18:45:49 -0700

    Initial Vue.js setup

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 This application represents an updated version of https://www.westjet.com/en-ca/flights/route-map
 
 It utilizes tools such as Leaflet for the Map API as well as Vue.js for the framework of the front end!
+
+Setup the project (must have npm installed prior to the following commands):
+
+npm install -g @vue/cli
+
---
+++ wjroutemap/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to be like your Vue.js daddy,<br>
+      check out the
+      <a href="https://www.linkedin.com/in/noah-bradley-6325a21b3/" target="_blank" rel="noopener">link here</a>.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
