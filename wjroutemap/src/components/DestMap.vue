295c222adf23c5d719b22e400a02a18077690853
Ionman <lucasion2001@gmail.com>
2023-03-12 15:32:25 -0600

    Attempted to add price functionality
    Still a work in progress

--- wjroutemap/src/components/DestMap.vue
+++ wjroutemap/src/components/DestMap.vue
@@ -1,78 +1,148 @@
 <template>
-  <div style="height:600px; width:800px">
-
+  <div style="height:1000px; width:1300px">
+ 
+ 
     <l-map ref="map" :zoom="zoom" :center="center" maxZoom="7" minZoom="4">
-      <l-tile-layer url="https://tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png?apikey=9b2313ed32304004a51c1494aedf88db" layer-type="base" name="OpenStreetMap"></l-tile-layer>
+      <l-tile-layer
+        url="https://tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png?apikey=9b2313ed32304004a51c1494aedf88db"
+        layer-type="base" name="OpenStreetMap"></l-tile-layer>
       <l-marker :key="index" v-for="(dest, index) in dests" :lat-lng="latLng(dest.latitude, dest.longitude)">
         <l-icon :icon-size="iconSize" :icon-url="icon"></l-icon>
-        <l-popup> You have selected {{dest.name}}'s airport </l-popup>
-        <l-tooltip> 
-          {{ dest.name }}, {{dest.countryName}}</l-tooltip>
+        <l-popup> You have selected {{ dest.name }}'s airport <br>
+         
+          Price: {{ dispPrice(dest.code) }}
+        </l-popup>
+        <l-tooltip>
+          {{ dest.name }}, {{ dest.countryName }}, {{getPrice(dest.code)}}</l-tooltip>
       </l-marker>
+ 
+ 
+ 
+ 
     </l-map>
   </div>
-</template>
-  
-<script>
-  import "leaflet/dist/leaflet.css";
-  import {
+ </template>
+  <script>
+ import "leaflet/dist/leaflet.css";
+ import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LIcon,
+  LTooltip,
+  LPopup,
+ } from "@vue-leaflet/vue-leaflet";
+ import westjet from '../assets/westjet.png'
+ export default {
+  name: 'DestMap',
+  props: {
+    dests: Array
+  },
+  components: {
     LMap,
     LTileLayer,
     LMarker,
     LIcon,
-    LTooltip,
     LPopup,
-  } from "@vue-leaflet/vue-leaflet";
-  import westjet from '../assets/westjet.png'
-  export default {
-    name: 'DestMap',
-    props: {
-      dests: Array
+    LTooltip,
+  },
+  data() {
+    return {
+      url: 'https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=9b2313ed32304004a51c1494aedf88db',
+      attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 4,
+      center: [47.313220, -1.319482], //default
+      markerLatLngBob: [47.313220, -17.319482],
+      icon: westjet,
+      iconSize: [40, 25],
+    };
+  },
+  methods: {
+    latLng: function (lat, lng) {
+ 
+ 
+      return [lat, lng]
     },
-    components: {
-      LMap,
-      LTileLayer,
-      LMarker,
-      LIcon,
-      LPopup,
-      LTooltip,
+    getLocation: function () {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          var latitude = position.coords.latitude;
+          var longitude = position.coords.longitude;
+          this.center = [latitude, longitude]
+        });
+      }
     },
-    data() {
-      return {
-        url: 'https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=9b2313ed32304004a51c1494aedf88db',
-        attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-        zoom: 4,
-        center: [47.313220, -1.319482], //default
-        markerLatLngBob: [47.313220, -17.319482],
-        icon: westjet,
-        iconSize: [40, 25],
-      };
+    dispPrice: async function (cityCode){
+      const result = await this.getPrice(cityCode);
+      console.log(result)
+ 
+ 
+ 
+ 
     },
-    methods: {
-      latLng: function(lat, lng) {
-        return [lat, lng]
-      },
-      getLocation: function() {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition((position) => {
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
-            this.center = [latitude, longitude]
-          });
+    getPrice: async function (destination) {
+ 
+ 
+      let accesscode = "YYC"+ destination
+ 
+ 
+      let value = ""
+ 
+ 
+      const axios = require('axios');
+ 
+ 
+      const params = new URLSearchParams();
+      params.append('o', 'YYC');
+      console.log(destination)
+      params.append('d', destination);
+      params.append('sourceCountryCode', 'CA');
+      params.append('rangeStartOffset', '30');
+      params.append('rangeEndOffset', '60');
+      params.append('iwe', '1');
+      params.append('iwe', '2');
+      params.append('iwe', '3');
+      params.append('iwe', '4');
+      params.append('iwe', '5');
+     
+ 
+ 
+      const response = await axios.get('https://api.westjet.com/price-points/v1/retail', {
+        params: params,
+        headers: {
+          'accept': 'application/json'
         }
-      },
+      }).then(response => value = (response.data[accesscode][0].price)).catch(error => {
+   console.log(error)
+ });
+       
+ 
+ 
+      if (value == 'undefined'){
+        value = 'Price Not Available'
+      }
+     
+     
+ 
+ 
+     
     },
-    mounted() {
-      this.getLocation()
-    }
-  };
-</script>
-  
-<style>
-  .leaflet-container {
-    border-radius: 25px;
-  }
-</style>
-
-    
-    
+  },
+  mounted() {
+    this.getLocation()
+ 
+ 
+  },
+ };
+ </script>
+  <style>
+ .leaflet-container {
+  border-radius: 25px;
+ }
+ </style>
+ 
+ 
+   
+   
+ 
+ 
