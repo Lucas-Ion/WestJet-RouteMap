e9d5e5d04222c8ba796aa7e0b3c0ad3bbdadaa8f
Ionman <lucasion2001@gmail.com>
2023-03-12 15:24:02 -0600

    Attempted to add price functionality
    Still a work in progress

--- wjroutemap/src/components/RouteMap.vue
+++ wjroutemap/src/components/RouteMap.vue
@@ -11,6 +11,7 @@
     </div>
     <!-- row -->
     <div class="row">
+<<<<<<< Updated upstream
       <div class="col-6">
         <h1 class="mt-5 mb-5">List</h1>
         <DestList :dests="dests" />
@@ -20,6 +21,11 @@
       <!-- left column -->
       <div class="col-6">
 
+=======
+
+      <!-- left column -->
+      <div class="ml-5 col-12">
+>>>>>>> Stashed changes
         <h1 class="mt-5 mb-5">Map</h1>
         <DestMap :dests="dests"/>
         <!-- Insert Map Here -->
@@ -40,7 +46,10 @@ import DestList from './DestList.vue';
 import DestMap from './DestMap.vue';
 /* eslint-disable */
 import axios from 'axios';
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 // https://api.westjet.com/destination-service/v1/en-CA/origins/YYC/destinations/westjet-only
 export default {
   name: 'RouteMap',
@@ -53,7 +62,10 @@ export default {
     DestList,
     DestMap,
   },
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
   mounted: function () {
     console.log('mounted');
     axios.get('https://api.westjet.com/destination-service/v1/en-CA/airports')
@@ -61,16 +73,47 @@ export default {
         console.log(r.data.airports)
         this.dests = r.data.airports
       })
+<<<<<<< Updated upstream
       axios.get('https://api.westjet.com/destination-service/v1/en-CA/airports')
       .then((r) => {
         console.log(r.data.airports)
        
       })
   }
+=======
+  }
+//   mounted: function () {
+//     // const axios = require('axios');
+
+//     // const params = new URLSearchParams();
+//     // params.append('o', 'YEG');
+//     // params.append('d', 'YYC');
+//     // params.append('d', 'YYZ');
+//     // params.append('sourceCountryCode', 'CA');
+//     // params.append('rangeStartOffset', '30');
+//     // params.append('rangeEndOffset', '60');
+//     // params.append('iwe', '1');
+//     // params.append('iwe', '2');
+//     // params.append('iwe', '3');
+//     // params.append('iwe', '4');
+//     // params.append('iwe', '5');
+
+//     // const response = axios.get('https://api.westjet.com/price-points/v1/retail', {
+//     //   params: params,
+//     //   headers: {
+//     //     'accept': 'application/json'
+//     //   }
+//     // });
+
+    
+//   },
+
+>>>>>>> Stashed changes
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
+<style lang="scss" scoped></style>
 
-</style>
+
+
